7aad8c6e561714cf8d9bf21a3e79160edf2d6527
YousefYousef0 <jusefjuju@gmail.com>
2025-01-19 04:05:58 +0100

    d

--- assats/scripts/index.js
+++ assats/scripts/index.js
@@ -2,76 +2,80 @@
 
 
 //typeStart
-    let TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
+let TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+    let i = this.loopNum % this.toRotate.length;
+    let fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    }
+    else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = `<span class="wrap">Hello I'm ${this.txt}</span>`;
+
+    let that = this;
+    let delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    }
+    else if (this.isDeleting && this.txt === '') {
         this.isDeleting = false;
-    };
-  
-    TxtType.prototype.tick = function() {
-        let i = this.loopNum % this.toRotate.length;
-        let fullTxt = this.toRotate[i];
+        this.loopNum++;
+        delta = 500;
+    }
 
-        if(this.isDeleting){
-            this.txt = fullTxt.substring(0, this.txt.length - 1);
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    let elements = document.getElementsByClassName('typewrite');
+    for (let i = 0; i < elements.length; i++) {
+        let toRotate = elements[i].getAttribute('data-type');
+        let period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
         }
-        else{
-            this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
+    }
 
-        this.el.innerHTML = `<span class="wrap">Hello I'm ${this.txt}</span>`;
-
-        let that = this;
-        let delta = 200 - Math.random() * 100;
-
-        if (this.isDeleting) { delta /= 2; }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-            delta = this.period;
-            this.isDeleting = true;
-        }
-        else if (this.isDeleting && this.txt === '') {
-            this.isDeleting = false;
-            this.loopNum++;
-            delta = 500;
-        }
-
-        setTimeout(function() {
-            that.tick();
-        }, delta);
-    };
-  
-    window.onload = function() {
-        let elements = document.getElementsByClassName('typewrite');
-        for (let i=0; i<elements.length; i++) {
-            let toRotate = elements[i].getAttribute('data-type');
-            let period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-                new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-
-        let css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.04em solid #fff}";
-        document.body.appendChild(css);
-    };
-  //typeEnd
+    let css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.04em solid #fff}";
+    document.body.appendChild(css);
+};
+//typeEnd
 
 let Lampa = document.querySelector(".Lampa");
 let lampa3 = document.querySelector("#lampa3");
 let toggle = true;
-Lampa.onclick = function(){
-    if(toggle == true){
+Lampa.onclick = function () {
+    if (toggle == true) {
         lampa3.style.fill = "#EFE799";
+        document.body.classList.remove("lightMode");
+        document.body.classList.add("darkMode");
         toggle = false;
     }
-    else{
+    else {
         lampa3.style.fill = "#545454";
+        document.body.classList.add("lightMode");
+        document.body.classList.remove("darkMode");
         toggle = true;
     }
 }
@@ -79,41 +83,41 @@ Lampa.onclick = function(){
 let navLeftA = document.querySelectorAll(".navLeft a");
 let footerHome = document.querySelectorAll(".footerHome");
 
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
     let scrollY = window.scrollY || window.pageYOffset;
     if (scrollY < 780) {
         navLeftA[0].classList.add("activate");
         navLeftA[1].classList.remove("activate");
         navLeftA[2].classList.remove("activate");
     }
-    else if(scrollY === document.documentElement.scrollHeight - window.innerHeight){
+    else if (scrollY === document.documentElement.scrollHeight - window.innerHeight) {
         navLeftA[0].classList.remove("activate");
         navLeftA[1].classList.remove("activate");
         navLeftA[2].classList.add("activate");
     }
-    else{
+    else {
         navLeftA[0].classList.remove("activate");
         navLeftA[1].classList.add("activate");
         navLeftA[2].classList.remove("activate");
     }
 });
-navLeftA[0].addEventListener("click", function() {
+navLeftA[0].addEventListener("click", function () {
     const Home = document.getElementById("Home");
     Home.scrollIntoView({ behavior: "smooth" });
 });
-navLeftA[1].addEventListener("click", function() {
+navLeftA[1].addEventListener("click", function () {
     const Works = document.getElementById("Works");
     Works.scrollIntoView({ behavior: "smooth" });
 });
-navLeftA[2].addEventListener("click", function() {
+navLeftA[2].addEventListener("click", function () {
     const Contact = document.getElementById("Contact");
     Contact.scrollIntoView({ behavior: "smooth" });
 });
-footerHome[0].addEventListener("click", function() {
+footerHome[0].addEventListener("click", function () {
     const Home = document.getElementById("Home");
     Home.scrollIntoView({ behavior: "smooth" });
 });
-footerHome[1].addEventListener("click", function() {
+footerHome[1].addEventListener("click", function () {
     const Home = document.getElementById("Home");
     Home.scrollIntoView({ behavior: "smooth" });
 });
@@ -138,14 +142,14 @@ function initializeSlider(sliderClassName, circleClassName, imgClassName) {
         circles[nextIndex].classList.remove("fa-regular");
 
         img.classList.add("fade-out");
-        setTimeout(function() {
-            img.src = `assats/images/p${sliderClassName.slice(7)}/Proj${sliderClassName.slice(-1)}img${nextIndex + 1}.jpg`;
+        setTimeout(function () {
+            img.src = `https://yousefyousef0.github.io/Portfolio/assats/images/p${sliderClassName.slice(7)}/Proj${sliderClassName.slice(-1)}img${nextIndex + 1}.jpg`;
             img.classList.remove("fade-out");
         }, 500);
         currentIndex = nextIndex;
     }
     for (let i = 0; i < circles.length; i++) {
-        circles[i].addEventListener("click", function() {
+        circles[i].addEventListener("click", function () {
             switchToNext();
         });
     }
